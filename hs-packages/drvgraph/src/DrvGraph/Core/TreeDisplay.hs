925a807177bf0eac69903b2291f5f7923e9a7f01
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-09-11 22:22:30 +0800

    Core: Add tree representation of dependency graph

---
+++ hs-packages/drvgraph/src/DrvGraph/Core/TreeDisplay.hs
@@ -0,0 +1,117 @@
+module DrvGraph.Core.TreeDisplay
+    ( StoreObjectTree (..)
+    , DerivationTree (..)
+    , toDisplayTree
+    ) where
+
+import Control.Applicative (empty)
+import Control.Monad.State.Strict (State, evalState, gets, modify)
+import Control.Monad.Trans.Maybe (MaybeT (..))
+import Data.Map qualified as Map
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Optics ((%~), (^.))
+import Optics.TH (makeFieldLabelsNoPrefix)
+
+import DrvGraph.Core.Model.DepGraph (DepGraph, ObjNode (ObjExisted, ObjUnbuilt, ObjUnsynced))
+import DrvGraph.Core.Model.DepGraph qualified as DepGraph
+import DrvGraph.Core.Model.DerivingPath (DerivingPath)
+import DrvGraph.Core.Model.StoreObjectPath (StoreObjectPath)
+
+data StoreObjectTree
+    = StObjTreeExisted
+        { objPath :: StoreObjectPath
+        }
+    | StObjTreeUnsynced
+        { objPath :: StoreObjectPath
+        , refChildren :: [StoreObjectTree]
+        }
+    | StObjTreeUnbuilt
+        { objPath :: StoreObjectPath
+        , drvChild :: DerivationTree
+        }
+    | StObjTreeUnbuiltWithDrv
+        { objPath :: StoreObjectPath
+        , drvPath :: DerivingPath
+        , objChildren :: [StoreObjectTree]
+        }
+    | StObjTreeVisited
+        { objPath :: StoreObjectPath
+        }
+    deriving (Eq, Show)
+
+data DerivationTree
+    = DrvTreeUnbuilt
+        { drvPath :: DerivingPath
+        , objChildren :: [StoreObjectTree]
+        }
+    | DrvTreeVisited
+        { drvPath :: DerivingPath
+        }
+    deriving (Eq, Show)
+
+makeFieldLabelsNoPrefix ''StoreObjectTree
+makeFieldLabelsNoPrefix ''DerivationTree
+
+data ToDisplayTreeState = ToDisplayTreeState
+    { visitedObjPaths :: Set StoreObjectPath
+    , visitedDrvPaths :: Set DerivingPath
+    }
+
+makeFieldLabelsNoPrefix ''ToDisplayTreeState
+
+-- | Convert a traversal from a @StoreObjectPath@ in a @DepGraph@ to a tree
+-- structure for displaying.
+toDisplayTree :: DepGraph -> StoreObjectPath -> Maybe StoreObjectTree
+toDisplayTree graph path = evalState (runMaybeT (recurseStoreObject graph path)) initial
+  where
+    initial =
+        ToDisplayTreeState
+            { visitedObjPaths = Set.empty
+            , visitedDrvPaths = Set.empty
+            }
+
+recurseStoreObject :: DepGraph -> StoreObjectPath -> MaybeT (State ToDisplayTreeState) StoreObjectTree
+recurseStoreObject graph objPath = do
+    isVisited <- gets $ Set.member objPath . (^. #visitedObjPaths)
+    if isVisited
+        then pure StObjTreeVisited{objPath}
+        else do
+            modify $ #visitedObjPaths %~ Set.insert objPath
+
+            objNode <- ofMaybe $ DepGraph.lookupObjNode objPath graph
+            case objNode of
+                ObjExisted -> pure StObjTreeExisted{objPath}
+                ObjUnsynced{refPaths} -> recurseForUnsynced refPaths
+                ObjUnbuilt{drvPath} -> recurseForUnbuilt drvPath
+  where
+    recurseForUnsynced refPaths = do
+        refChildren <- traverse (recurseStoreObject graph) (Set.toList refPaths)
+        pure StObjTreeUnsynced{objPath, refChildren}
+
+    recurseForUnbuilt drvPath = do
+        (drvNode, indegree) <- ofMaybe $ DepGraph.lookupDrvNodeAndIndegree drvPath graph
+        if indegree > 1
+            then do
+                drvChild <- recurseDerivation graph drvPath
+                pure StObjTreeUnbuilt{objPath, drvChild}
+            else do
+                let refPaths = Map.keys (drvNode ^. #inputObjPaths)
+                objChildren <- traverse (recurseStoreObject graph) refPaths
+                pure StObjTreeUnbuiltWithDrv{objPath, drvPath, objChildren}
+
+recurseDerivation :: DepGraph -> DerivingPath -> MaybeT (State ToDisplayTreeState) DerivationTree
+recurseDerivation graph drvPath = do
+    isVisited <- gets $ Set.member drvPath . (^. #visitedDrvPaths)
+    if isVisited
+        then pure DrvTreeVisited{drvPath}
+        else do
+            modify $ #visitedDrvPaths %~ Set.insert drvPath
+
+            drvNode <- ofMaybe $ DepGraph.lookupDrvNode drvPath graph
+            let refPaths = Map.keys (drvNode ^. #inputObjPaths)
+            objChildren <- traverse (recurseStoreObject graph) refPaths
+            pure DrvTreeUnbuilt{drvPath, objChildren}
+
+ofMaybe :: (Monad m) => Maybe a -> MaybeT m a
+ofMaybe = maybe empty pure
---
+++ hs-packages/drvgraph/test/DrvGraph/Core/TreeDisplayTest.hs
@@ -0,0 +1,139 @@
+module DrvGraph.Core.TreeDisplayTest
+    ( unit_toDisplayTreeAllBranches
+    , unit_toDisplayTreeUnbuiltWithDrvLeaf
+    ) where
+
+import Data.Function ((&))
+import Data.Map.Strict qualified as Map
+import Data.Set qualified as Set
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Test.Tasty.HUnit ((@?=))
+
+import DrvGraph.Core.Model.DepGraph (DepGraph, DrvNode (..), ObjNode (..))
+import DrvGraph.Core.Model.DepGraph qualified as DepGraph
+import DrvGraph.Core.Model.DerivingPath (DerivingPath)
+import DrvGraph.Core.Model.DerivingPath qualified as DerivingPath
+import DrvGraph.Core.Model.StoreObjectPath (StoreObjectPath)
+import DrvGraph.Core.Model.StoreObjectPath qualified as StoreObjectPath
+import DrvGraph.Core.TreeDisplay
+    ( DerivationTree (..)
+    , StoreObjectTree (..)
+    , toDisplayTree
+    )
+
+fakeHash :: Int -> Text
+fakeHash i = Text.replicate 32 (Text.singleton (alphaNums !! i))
+  where
+    alphaNums = "0123456789abcdfghijklmnpqrsvwxyz"
+
+drvPathOf :: Int -> Text -> DerivingPath
+drvPathOf n name = DerivingPath.uncheckedText (fakeHash n <> "-" <> name <> ".drv")
+
+storeObjectPathOf :: Int -> Text -> StoreObjectPath
+storeObjectPathOf n name = StoreObjectPath.uncheckedText (fakeHash n <> "-" <> name)
+
+objRoot, objA, objB, objC, objD, objE, objG, objR, objO :: StoreObjectPath
+objRoot = storeObjectPathOf 0 "root"
+objA = storeObjectPathOf 1 "a"
+objB = storeObjectPathOf 2 "b"
+objC = storeObjectPathOf 3 "c"
+objD = storeObjectPathOf 4 "d"
+objE = storeObjectPathOf 5 "e"
+objG = storeObjectPathOf 6 "g"
+objR = storeObjectPathOf 7 "r"
+objO = storeObjectPathOf 8 "o"
+
+drvRoot, drvA, drvShared, drvE, drvC, drvG, drvR, drvO :: DerivingPath
+drvRoot = drvPathOf 0 "root"
+drvA = drvPathOf 1 "a"
+drvShared = drvPathOf 2 "shared"
+drvE = drvPathOf 3 "e"
+drvC = drvPathOf 4 "c"
+drvG = drvPathOf 5 "g"
+drvR = drvPathOf 6 "r"
+drvO = drvPathOf 7 "o"
+
+applyObjNodeInsertions :: [(StoreObjectPath, ObjNode)] -> DepGraph -> DepGraph
+applyObjNodeInsertions pairs graph = foldr (uncurry DepGraph.insertObjNode) graph pairs
+
+applyDrvNodeInsertions :: [(DerivingPath, DrvNode)] -> DepGraph -> DepGraph
+applyDrvNodeInsertions pairs graph = foldr (uncurry DepGraph.insertDrvNode) graph pairs
+
+unit_toDisplayTreeAllBranches :: IO ()
+unit_toDisplayTreeAllBranches = do
+    let graph =
+            DepGraph.empty
+                & applyObjNodeInsertions
+                    [ (objRoot, ObjUnbuilt{drvPath = drvRoot})
+                    , (objA, ObjUnsynced{drvPath = drvA, refPaths = Set.singleton objC})
+                    , (objB, ObjUnbuilt{drvPath = drvShared})
+                    , (objC, ObjExisted)
+                    , (objD, ObjUnbuilt{drvPath = drvShared})
+                    , (objE, ObjUnsynced{drvPath = drvE, refPaths = Set.empty})
+                    , (objG, ObjExisted)
+                    ]
+                & applyDrvNodeInsertions
+                    [ (drvRoot, DrvNode{inputObjPaths = Map.fromList [(objA, (drvA, "out")), (objB, (drvShared, "out")), (objD, (drvShared, "dev")), (objE, (drvE, "out"))]})
+                    , (drvA, DrvNode{inputObjPaths = Map.empty})
+                    , (drvShared, DrvNode{inputObjPaths = Map.fromList [(objC, (drvC, "out")), (objG, (drvG, "out"))]})
+                    ]
+
+    let Just actual = toDisplayTree graph objRoot
+
+    actual
+        @?= StObjTreeUnbuiltWithDrv
+            { objPath = objRoot
+            , drvPath = drvRoot
+            , objChildren =
+                [ StObjTreeUnsynced
+                    { objPath = objA
+                    , refChildren = [StObjTreeExisted{objPath = objC}]
+                    }
+                , StObjTreeUnbuilt
+                    { objPath = objB
+                    , drvChild =
+                        DrvTreeUnbuilt
+                            { drvPath = drvShared
+                            , objChildren =
+                                [ StObjTreeVisited{objPath = objC}
+                                , StObjTreeExisted{objPath = objG}
+                                ]
+                            }
+                    }
+                , StObjTreeUnbuilt
+                    { objPath = objD
+                    , drvChild = DrvTreeVisited{drvPath = drvShared}
+                    }
+                , StObjTreeUnsynced
+                    { objPath = objE
+                    , refChildren = []
+                    }
+                ]
+            }
+
+unit_toDisplayTreeUnbuiltWithDrvLeaf :: IO ()
+unit_toDisplayTreeUnbuiltWithDrvLeaf = do
+    let graph =
+            DepGraph.empty
+                & applyObjNodeInsertions
+                    [ (objR, ObjUnsynced{drvPath = drvR, refPaths = Set.singleton objO})
+                    , (objO, ObjUnbuilt{drvPath = drvO})
+                    ]
+                & applyDrvNodeInsertions
+                    [ (drvO, DrvNode{inputObjPaths = Map.empty})
+                    ]
+
+    let Just actual = toDisplayTree graph objR
+
+    actual
+        @?= StObjTreeUnsynced
+            { objPath = objR
+            , refChildren =
+                [ StObjTreeUnbuiltWithDrv
+                    { objPath = objO
+                    , drvPath = drvO
+                    , objChildren = []
+                    }
+                ]
+            }
