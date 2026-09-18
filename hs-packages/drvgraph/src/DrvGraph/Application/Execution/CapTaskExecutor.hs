877f3b7d6119348f089014f10a9c6ba67afe1fb6
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-09-19 00:50:43 +0800

    Application: Change task executor to thread pool based impl

--- hs-packages/drvgraph/src/DrvGraph/Application/Execution/CapTaskExecutor.hs
+++ hs-packages/drvgraph/src/DrvGraph/Application/Execution/CapTaskExecutor.hs
@@ -2,22 +2,51 @@ module DrvGraph.Application.Execution.CapTaskExecutor
     ( withTaskExecutorImpl
     ) where
 
-import Control.Monad.IO.Class (MonadIO)
-import Data.Sequence (ViewL (EmptyL, (:<)), (|>))
-import Data.Sequence qualified as Seq
-import UnliftIO.IORef qualified as IORef
+import Control.Exception.Safe (MonadCatch, MonadMask, SomeException, bracket, throw, tryAny)
+import UnliftIO (MonadIO, MonadUnliftIO)
+import UnliftIO.Async qualified as Async
+import UnliftIO.Concurrent qualified as Concurrent
+import UnliftIO.STM (TQueue, TVar)
+import UnliftIO.STM qualified as Stm
 
-withTaskExecutorImpl :: (MonadIO m) => ((m a -> m (), m (Maybe a)) -> m r) -> m r
+withTaskExecutorImpl :: (MonadMask m, MonadUnliftIO m) => ((m a -> m (), m (Maybe a)) -> m r) -> m r
 withTaskExecutorImpl action = do
-    queue <- IORef.newIORef Seq.empty
-    action (submit queue, await queue)
+    taskTx <- Stm.atomically Stm.newTQueue
+    resultRx <- Stm.atomically Stm.newTQueue
+    unfinishedTasks <- Stm.atomically $ Stm.newTVar 0
+
+    num <- Concurrent.getNumCapabilities
+    bracket
+        (traverse Async.async . replicate num $ taskWorker taskTx resultRx unfinishedTasks)
+        (traverse Async.cancel)
+        (const $ action (submit taskTx unfinishedTasks, await resultRx unfinishedTasks))
   where
-    submit queue task = do
-        IORef.atomicModifyIORef' queue (\q -> (q |> task, ()))
-    await queue = do
-        q <- IORef.readIORef queue
-        case Seq.viewl q of
-            EmptyL -> pure Nothing
-            task :< newQueue -> do
-                IORef.atomicModifyIORef' queue (const (newQueue, ()))
-                Just <$> task
+    submit taskTx unfinishedTasks task = do
+        Stm.atomically $ do
+            Stm.writeTQueue taskTx task
+            Stm.modifyTVar' unfinishedTasks (+ 1)
+
+    await resultRx unfinishedTasks = do
+        res <- Stm.atomically $ do
+            hasUnfinished <- (> 0) <$> Stm.readTVar unfinishedTasks
+            if hasUnfinished
+                then Just <$> Stm.readTQueue resultRx
+                else Stm.tryReadTQueue resultRx
+
+        case res of
+            Nothing -> pure Nothing
+            Just (Left ex) -> throw ex
+            Just (Right val) -> pure $ Just val
+
+taskWorker
+    :: (MonadCatch m, MonadIO m)
+    => TQueue (m a) -> TQueue (Either SomeException a) -> TVar Int -> m ()
+taskWorker taskRx resultTx unfinishedTasks = loop
+  where
+    loop = do
+        front <- Stm.atomically $ Stm.readTQueue taskRx
+        res <- tryAny front
+        Stm.atomically $ do
+            Stm.writeTQueue resultTx res
+            Stm.modifyTVar' unfinishedTasks (\x -> x - 1)
+        loop
