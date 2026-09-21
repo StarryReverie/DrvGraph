6048dae146edf9dfda659b0df7ba173d11176c4c
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-09-21 16:44:47 +0800

    Core/Model: Rename derivation file fixtures

--- hs-packages/drvgraph/test/DrvGraph/Core/Model/DerivationTest.hs
+++ hs-packages/drvgraph/test/DrvGraph/Core/Model/DerivationTest.hs
@@ -17,7 +17,7 @@ import DrvGraph.Core.Model.StoreObjectPath qualified as StoreObjectPath
 
 unit_parseHello :: IO ()
 unit_parseHello = do
-    let input = TextEncoding.decodeUtf8 $(Embed.embedFileRelative "data/vlp8xby1jjmif5hdsck5vfdq4fljpvli-hello-2.12.3.drv")
+    let input = TextEncoding.decodeUtf8 $(Embed.embedFileRelative "data/hello-2.12.3.drv")
 
     let Right actual = Megaparsec.runParser Derivation.parse "" input
 
@@ -62,7 +62,7 @@ unit_parseHello = do
 
 unit_parseHelloSource :: IO ()
 unit_parseHelloSource = do
-    let input = TextEncoding.decodeUtf8 $(Embed.embedFileRelative "data/fmyr3q5fikw8g50g1xf2vgs10hisxck9-hello-2.12.3.tar.gz.drv")
+    let input = TextEncoding.decodeUtf8 $(Embed.embedFileRelative "data/hello-2.12.3.tar.gz.drv")
 
     let Right actual = Megaparsec.runParser Derivation.parse "" input
 
